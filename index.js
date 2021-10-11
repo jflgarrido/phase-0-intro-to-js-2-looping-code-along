64d63548f30debfe4f5583b6e5690161594b79a2
Julius Garrido <jflgarrido@gmail.com>
2021-10-11 13:36:45 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names) {
+    let birthday = []
+  for (let i = 0; i < names.length; i++) {
+    birthday.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}
+  return birthday;
+}
+
+writeCards(names);
+
+let number = 10
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--)
+    }
+    return number;
+}
+countDown(number);
